5634b1a89d0c0a8046f26a576284edb62db8ca8f
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-04-19 10:57:00 +0300

    minimal example

---
+++ WinformHostGViewer/StaticGraph.cs
@@ -0,0 +1,34 @@
+﻿namespace WinformHostGViewer
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("c94de928-222e-416a-b929-55a31c304676")]
+    public class StaticGraph : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StaticGraph"/> class.
+        /// </summary>
+        public StaticGraph() : base(null)
+        {
+            this.Caption = "StaticGraph";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new StaticGraphControl();
+        }
+    }
+}
---
+++ WinformHostGViewer/StaticGraphControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms.Integration;
+using Microsoft.Msagl.GraphViewerGdi;
+
+namespace WinformHostGViewer
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for StaticGraphControl.
+    /// </summary>
+    public partial class StaticGraphControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StaticGraphControl"/> class.
+        /// </summary>
+        public StaticGraphControl()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private static GViewer viewer;
+
+        static StaticGraphControl()
+        {
+
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            //create the graph content 
+            graph.AddEdge("A", "B");
+            graph.Directed = false;
+            //graph.AddEdge(, )
+            //edge.Attr.ArrowHeadAtTarget=ArrowStyle.None
+            graph.AddEdge("B", "C");
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            viewer = new GViewer();
+            viewer.Graph = graph;
+            Debug.WriteLine("Initialized graph");
+            Debug.WriteLine(graph.GeometryGraph);
+        }
+
+        public WindowsFormsHost Graph
+        {
+            get
+            {
+                Debug.WriteLine(viewer.Graph);
+                return new WindowsFormsHost() { Child = viewer };
+            }
+        }
+    }
+}
